0f9c381be96155fae7a3504317b8a2a10f5ecd41
Tomasz Klim <github@tomaszklim.pl>
2016-01-01 18:04:22 +0100

    fixed uninstall script

--- uninstall.sh
+++ uninstall.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-if grep -q CATALINA_HOME /etc/crontab; then
-	sed -i -e "/CATALINA_HOME/d" /etc/crontab
+if grep -q CATALINA_HOME /etc/environment; then
+	sed -i -e "/CATALINA_HOME/d" /etc/environment
 fi
